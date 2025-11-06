0c037d66a5a8e7ea6840ccdd206b045a3c4b83ef
gulnaz <gulnazhakimova70@gmail.com>
2025-11-06 13:49:36 +0300

    fixed login#2

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,5 +1,27 @@
+import { useEffect } from 'react';
+
 const Login = () => {
-  return <div></div>;
+  useEffect(() => {
+    window.onTelegramAuth = (user) => {
+      alert(
+        `Logged in as ${user.first_name} ${user.last_name} (${user.id}${
+          user.username ? ', @' + user.username : ''
+        })`
+      );
+    };
+
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.async = true;
+    script.setAttribute('data-telegram-login', 'GgumiblvBot');
+    script.setAttribute('data-size', 'medium');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-request-access', 'write');
+
+    document.getElementById('telegram-login-container').appendChild(script);
+  }, []);
+
+  return <div id="telegram-login-container"></div>;
 };
 
 export default Login;
