6c8b95197edb16627852fb818740debdb3c4dfb0
gulnaz <gulnazhakimova70@gmail.com>
2025-11-06 13:37:43 +0300

    fixed login

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,30 +1,5 @@
-// import { retrieveRawInitData } from '@telegram-apps/sdk';
-
-// const initDataRaw = retrieveRawInitData();
-
-// const apiUrl = 'https://my-bot-backend-1.onrender.com';
-
 const Login = () => {
-  // const handleClick = () => {
-  //   fetch(`${apiUrl}/login`, {
-  //     method: 'POST',
-  //     headers: {
-  //       Authorization: `tma ${initDataRaw}`
-  //     }
-  //   });
-  // };
-  return (
-    <div>
-      <script
-        async
-        src="https://telegram.org/js/telegram-widget.js?22"
-        data-telegram-login="GgumiblvBot"
-        data-size="medium"
-        data-auth-url="https://my-bot-backend-6ig5.onrender.com"
-        data-request-access="write"
-      ></script>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
